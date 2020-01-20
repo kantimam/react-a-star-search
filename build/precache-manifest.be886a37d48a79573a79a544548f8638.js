6ca7b379c8878d1d3baf05d0dc71029c72437161
Nyannazz <kantemir.imam@gmail.com>
2020-01-20 15:12:28 +0100

    gitignore fixed

---
+++ build/precache-manifest.be886a37d48a79573a79a544548f8638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895542b5b6703e623351ba9e184c796a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b7498dcd64ea9ea61b",
+    "url": "/static/css/main.001d40a3.chunk.css"
+  },
+  {
+    "revision": "5f890ef25ed3b29388a9",
+    "url": "/static/js/2.2862c3fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2862c3fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88b7498dcd64ea9ea61b",
+    "url": "/static/js/main.c35676e1.chunk.js"
+  },
+  {
+    "revision": "855c791edd562f0ad653",
+    "url": "/static/js/runtime-main.7f129797.js"
+  }
+]);
